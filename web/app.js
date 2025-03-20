24edce2de4b4796d769183080637b4626dcd2fce
Kapil Chaudhary <101615305+heykapil@users.noreply.github.com>
2025-03-20 13:35:12 +0530

    Update app.js

--- web/app.js
+++ web/app.js
@@ -2281,8 +2281,12 @@ if (typeof PDFJSDev === "undefined" || !PDFJSDev.test("MOZCENTRAL")) {
 if (typeof PDFJSDev === "undefined" || PDFJSDev.test("GENERIC")) {
   const HOSTED_VIEWER_ORIGINS = new Set([
     "null",
-    "http://mozilla.github.io",
-    "https://mozilla.github.io",
+    "http://heykapil.github.io",
+    "https://kapil.app",
+    "https://drive.kapil.app",
+    "https://file.kapil.app",
+    "https://pdf.kapil.app",
+    "https://stream.kapil.app"
   ]);
   // eslint-disable-next-line no-var
   var validateFileURL = function (file) {
